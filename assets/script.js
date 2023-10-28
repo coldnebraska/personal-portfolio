d0b0cbcbcb662bda4e4f6954d5b9f770656b0562
DBrush <coldnebraska@gmail.com>
2023-10-28 16:13:06 -0500

    added submission modal

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,26 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => alert("Form successfully submitted"))
+    .then(function() {
+        let modalBtn = $(".button")
+        let modal = $(".modal")
+        let close = $(".close")
+        let modalId = document.getElementById("modal")
+
+        modalBtn.click(function() {
+            modal.css("display", "block")
+        })
+
+        close.click(function() {
+            modal.css("display", "none")
+        })
+
+        window.addEventListener("click", function(event) {
+            if (event.target === modalId) {
+                modal.css("display", "none")
+            }
+        })
+    })
     .catch((error) => alert(error));
 };
 
